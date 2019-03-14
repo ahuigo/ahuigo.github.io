5e4f0c97f515940d96626e197797ec451c923d8a
ahuigo <a132811@gmail.com>
2019-03-14 15:55:16 +0800

    zshrc

--- tool/install/install-mac.sh
+++ tool/install/install-mac.sh
@@ -1,5 +1,15 @@
 # gnu
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew install wget gnu-sed
+brew install wget gnu-sed git
 brew install python3 
 brew cask install google-chrome iterm2 alfred squirrel
+# alfred
+## dict
+
+# squirrel
+
+# a.conf
+mkdir www
+git clone https://github.com/ahuigo/a www/a
+
+# 
