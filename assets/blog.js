ebbe6c3caaea3a7a4645f6dbd8a2dcef3085878f
ahuigo <1781999+ahuigo@users.noreply.github.com>
2024-06-26 21:35:27 +0800

    fix:css

--- assets/blog.js
+++ assets/blog.js
@@ -1,6 +1,5 @@
 import * as toclib from './toc.js';
-//import styles from "/assets/main.css" assert { type: "css" };
-import styles from "/assets/main.css";
+import styles from "/assets/main.css" assert { type: "css" };
 import "https://cdnjs.cloudflare.com/ajax/libs/vue/2.7.10/vue.min.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/vue-router/3.6.5/vue-router.min.js";
 document.adoptedStyleSheets = [...document.adoptedStyleSheets, styles];
